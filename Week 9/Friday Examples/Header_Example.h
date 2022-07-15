b454ff08da6719ea398da08c0c366e0755da918d
cassLaffan <poisonberryvines@gmail.com>
2022-07-15 12:20:22 -0400

    Adding the Friday example

---
+++ Week 9/Friday Examples/Header_Example.h
@@ -0,0 +1,17 @@
+/*
+* This week, we will work through creating a multi-file program.
+*
+* Program requirements:
+*
+* You have been tasked with creating a small program that tracks
+* pets at a kennel. It should have the following functionalities:
+*
+* 1) There should be a way to keep track of pets currently at the
+* kennel.
+* 2) There should be a way to keep track of pets who have been 
+* adopted.
+* 3) There should be a way to add pets to the kennel list, and to
+* mark whether or not they are adoptable.
+* Bonus: Try to make a file that allows you to persist these changes
+* between program runs.
+*/
